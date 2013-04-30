5b8a130c3a0b74205f52330175e1da897db2b82c
Anatoli Veselinov <oldfox1965@gmail.com>
2013-05-01 00:46:04 +0200

    Defining Calendar class

---
+++ src/org/fofo/entity/Calendar.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.fofo.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Anatoli Veselinov
+ */
+public /*abstract*/ class Calendar {
+    
+    private Competition competition;
+    private List<WeekMatches> calendar;
+    
+    public Calendar(){
+        calendar = new ArrayList();
+    }
+    
+    
+    
+    
+
+}
