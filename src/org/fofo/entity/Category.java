3f6ac96d7f814f1599d2190a11bba3b4b4095855
Josep M. Ribo <josepma.models@gmail.com>
2013-04-30 13:31:21 +0200

    Creation Category.java

---
+++ src/org/fofo/entity/Category.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fofo.entity;
+
+/**
+ *
+ * @author josepma
+ */
+public enum Category {
+  MALE,FEMALE,VETERAN;
+    
+    
+}
