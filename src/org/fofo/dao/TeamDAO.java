ecbb3e0b67a6d5e82bb35db8f360ee988bb5dcb0
Josep M. Ribo <josepmar@gmail.com>
2013-04-30 12:56:51 +0200

    dao+service

---
+++ src/org/fofo/dao/TeamDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fofo.dao;
+
+/**
+ *
+ * @author josepma
+ */
+public interface TeamDAO {
+    
+}
---
+++ src/org/fofo/services/management/ManagementService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fofo.services.management;
+
+/**
+ *
+ * @author josepma
+ */
+public class ManagementService {
+    
+}
