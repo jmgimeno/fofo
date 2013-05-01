59ed4f13e4e20e590f06361ca614d4098df90f3f
Josep M. Ribo <josepma.models@gmail.com>
2013-05-01 09:31:32 +0200

    Minor changes

---
+++ src/org/fofo/entity/Competition.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fofo.entity;
+
+/**
+ *
+ * @author josepma
+ */
+public class Competition {
+    
+}
