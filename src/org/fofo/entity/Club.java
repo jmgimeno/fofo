f12d7ea0e60b2b411d2ba8f1a84d9c2308b8aa10
Ivan <Ivan@192.168.0.194>
2013-05-01 16:29:01 +0200

    Definicio de la classe Club

--- src/org/fofo/entity/Club.java
+++ src/org/fofo/entity/Club.java
@@ -4,10 +4,43 @@
  */
 package org.fofo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author portatil
  */
 class Club {
+    private String name;
+    private String email;
+    private List<Team> teams = new ArrayList<Team>();
     
+    public Club(){
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
 }
