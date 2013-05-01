1f8a30de6c4195d68b9b566198f6209660ab9d1e
Ivan <Ivan@192.168.0.194>
2013-05-01 16:31:13 +0200

    Creacio de la classe enum Type

---
+++ src/org/fofo/entity/Type.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fofo.entity;
+
+/**
+ *
+ * @author Ivan
+ */
+public enum Type {
+    
+    LEAGUE, CUP
+    
+}
