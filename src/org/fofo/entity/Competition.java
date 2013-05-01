ab051ac5af2958b968e3dbe10830bfac25f87fc2
Ivan <Ivan@192.168.0.194>
2013-05-01 16:29:47 +0200

    Definicio de la classe Competition

--- src/org/fofo/entity/Competition.java
+++ src/org/fofo/entity/Competition.java
@@ -4,10 +4,75 @@
  */
 package org.fofo.entity;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author josepma
  */
 public class Competition {
+    
+    private Category category;
+    private Date inici;
+    private List<Team> teams = new ArrayList<Team>();
+    private int maxTeams;
+    private int minTeams;
+    private Type type;
+    
+    public Competition(){
+        
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Date getInici() {
+        return inici;
+    }
+
+    public void setInici(Date inici) {
+        this.inici = inici;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+
+    public int getMaxTeams() {
+        return maxTeams;
+    }
+
+    public void setMaxTeams(int maxTeams) {
+        this.maxTeams = maxTeams;
+    }
+
+    public int getMinTeams() {
+        return minTeams;
+    }
+
+    public void setMinTeams(int minTeams) {
+        this.minTeams = minTeams;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+    
+    
     
 }
