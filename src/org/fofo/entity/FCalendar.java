76a418c7cdd76d90cc86e412c71b9019ba67d4aa
Anatoli <anatoli.veselinov@gmail.com>
2013-05-06 18:54:00 +0200

    Redefinit FCalendar Renombrat FCalendarTest.java -> CalendarLeagueGenTest.java

--- src/org/fofo/entity/FCalendar.java
+++ src/org/fofo/entity/FCalendar.java
@@ -19,88 +19,39 @@ public class FCalendar {
     
     private Competition competition;
     private List<WeekMatches> calendar;
-    private int numTeams;
-    private LinkedList<Team> locals;   //LinkedList perque es comporta com una pila
-    private LinkedList<Team> visitants;
+    //private int numTeams;
     
-    public FCalendar(Competition c) throws Exception{
-        
-        competition = c;
-        numTeams = competition.getNumberOfTeams();
-        
-        checkSevenDaysMinAfterCompetition();        
-        checkNumOfTeamsIsInLimits();
-        
-        calendar = new ArrayList<WeekMatches>();        
-        
-        if(competition.getType()==Type.LEAGUE){
-            checkNumOfTeamsIsPair();
-            manageLeague();
-        }else if(competition.getType()==Type.CUP){
-            manageCup();
-        }else
-            throw new UnknownCompetitionTypeException(); 
-        
-        
+    public FCalendar(){
+        competition = null;
+        calendar = new ArrayList<WeekMatches>();
         
     }
     
-    private void manageLeague() throws Exception{
-        List<Team> tmp = competition.getTeams();
-        
-        //--->metode per remenar la llista<---
-        
-        initAssignation(tmp);
-        //1a volta
-        for(int i=0; i<numTeams; i++){  //n-1 jornades per volta
-            WeekMatches wm = buildWeekMatches();
-            calendar.add(wm);
-            rotateAssignation();            
-        }
+    public FCalendar(Competition c, List<WeekMatches> wm){
+        this.competition = c;
+        this.calendar = wm;
     }
     
-    private void initAssignation(List<Team> teams) {
-        //1a meitat: loclas
-        //2a meitat: visitants        
+    public FCalendar createCalendar(Competition c) throws Exception{
         
-        locals.addAll(teams.subList(0, (numTeams/2)-1));
-        visitants.addAll(teams.subList(numTeams/2, numTeams-1));
+        //return CalendarGen(c).getCalendar(); //Si CalendarGen es classe abstracta 
+        //Si es inteficie:
+        if(c.getType()==Type.LEAGUE)
+            return CalendarLeagueGen.getCalendar();
+        else if(c.getType()==Type.CUP)
+            return CalendarCupGen.getCalendar();
+        else
+            throw new UnknownCompetitionTypeException();
+        
     }
     
-    private void rotateAssignation() {
-        //Deixem fixat el 1r equip de locals i rotem cap a la dreta
-        
-        Team teamToLocals = visitants.removeFirst();
-        Team teamToVisitants = locals.removeLast();
-        
-        locals.add(1, teamToLocals);
-        visitants.add(teamToVisitants);
-        
-    }  
-
-    private WeekMatches buildWeekMatches() throws Exception{
-        WeekMatches wm = null;
-        
-        for(int i=0; i<numTeams/2; i++){  // n/2 partits per jornada
-            Match m = buildMatch(i);
-            wm.addMatch(m);
-        }
-        return wm;
+    public void setCompetition(Competition c){
+        this.competition = c;
     }
     
-    private Match buildMatch(int index) {
-        Team local = locals.get(index);
-        Team visitant = visitants.get(index);
-        
-        Date date = new Date();
-        //date = assignDate();
-        
-        return new Match(local,visitant);
-    }     
-
-    private Date assignDate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }   
+    public void setWeekMatches(List<WeekMatches> wm){
+        this.calendar = wm;
+    }
     
     public List<WeekMatches> getAllWeekMatches() {
         return calendar;
@@ -113,40 +64,7 @@ public class FCalendar {
     public int getNumOfWeekMatches(){
         return calendar.size();
     }
-
-    public List<Team> getLocals() {
-        return locals;
-    }
-
-    public List<Team> getVisitants() {
-        return visitants;
-    }     
-
-    private void checkSevenDaysMinAfterCompetition() throws Exception {
-        DateTime actual = new DateTime();
-        DateTime compDate = new DateTime(competition.getInici()); 
-        if((actual.getDayOfYear() - compDate.getDayOfYear()) < 7)
-            throw new MinimumDaysException("It must be a diference of 7 days as "
-                    + "minimum betwen Calendar creation and Competition creation"); 
-    }
-
-    private void checkNumOfTeamsIsPair() throws Exception {
-        if(numTeams%2 != 0)
-            throw new NumberOfTeamsException("This LEAGUE competition has not "
-                    + "a PAIR number of teams"); 
-    }
-
-    private void checkNumOfTeamsIsInLimits() throws Exception {
-        if(numTeams < competition.getMinTeams() 
-        && numTeams > competition.getMaxTeams())
-            throw new NumberOfTeamsException("This LEAGUE competition has not "
-                    + "the right number of teams betwen "
-                    +competition.getMinTeams()+" and "+competition.getMaxTeams()); 
-    }
-    
-    /*Jordio i Oriol*/
-    private void manageCup(){
         
-    } 
-
+    
+    
 }
