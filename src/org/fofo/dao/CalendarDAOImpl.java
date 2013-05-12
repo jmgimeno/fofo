a55e8f599ba1ef9a6e39c35d6613e5c54678409b
auc2 <auc2@auc2-Aspire-6530G.(none)>
2013-05-12 13:29:13 +0200

    Millores test CalendarDAO Impl

--- src/org/fofo/dao/CalendarDAOImpl.java
+++ src/org/fofo/dao/CalendarDAOImpl.java
@@ -11,10 +11,11 @@ import org.fofo.entity.WeekMatches;
  * @author Anton Urrea
  */
 public class CalendarDAOImpl implements CalendarDAO {
-    
+
     EntityManager em;
-    
+
     public void addCalendar(FCalendar cal) {
+        
         em.getTransaction().begin();
         for (int i = 0; i < cal.getNumOfWeekMatches(); i++) {
             addWeekMatches(cal.getAllWeekMatches().get(i));
@@ -22,15 +23,24 @@ public class CalendarDAOImpl implements CalendarDAO {
         em.persist(cal);
         em.getTransaction().commit();
     }
-    
+
     private void addWeekMatches(WeekMatches wm) {
+        
         for (int i = 0; i < wm.getListOfWeekMatches().size(); i++) {
             addMatch(wm.getListOfWeekMatches().get(i));
         }
         em.persist(wm);
     }
-    
+
     private void addMatch(Match match) {
         em.persist(match);
     }
+
+    Object get(int i,WeekMatches wm) {
+        
+        return wm.getListOfWeekMatches().get(i);       
+    }
+
+
+
 }
--- test/org/fofo/dao/CalendarDAOImplTest.java
+++ test/org/fofo/dao/CalendarDAOImplTest.java
@@ -4,9 +4,12 @@
  */
 package org.fofo.dao;
 
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import org.fofo.entity.FCalendar;
+import org.fofo.entity.Match;
+import org.fofo.entity.WeekMatches;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
@@ -15,9 +18,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 /**
  *
- * @author auc2
+ * @author David Hernández
+ * @author Anton Urrea
  */
 
 @RunWith(JMock.class)
@@ -31,29 +36,40 @@ public class CalendarDAOImplTest {
     
     CalendarDAOImpl calDAO;
     
+    Match match1, match2, match3, match4;
+    WeekMatches wm, wm2;
+    FCalendar cal;
     
-    
+
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         em = context.mock(EntityManager.class);
         transaction = context.mock(EntityTransaction.class);
+           
+        wm.addMatch(match1);
+        wm.addMatch(match2);
+        
+        wm2.addMatch(match3);
+        wm2.addMatch(match4);
           
     }
     
     
     @Test
     public void testAddMatches(){
-         
-        context.checking(new Expectations() {{             
+              
+        context.checking(new Expectations() {{                   
+            
+             for (int i = 0; i < cal.getNumOfWeekMatches(); i++) {      
            
-             //   oneOf(em).persist(em.get(0));              
-               // oneOf(em).persist(em.get(1));
+                oneOf(em).persist(calDAO.get(i, wm));         
+                oneOf(em).persist(calDAO.get(i, wm2)); 
                 oneOf(em).close();
+                
+                 }
         }});
     
         calDAO.addCalendar(fcal);       
     }
-    
-    
-    
+      
 }
