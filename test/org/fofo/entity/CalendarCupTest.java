ce251c325ca9d8e7c0704e37d2881ddd3f49f014
Jordi Niubo Pomar <jnp2@pl-cap018.udl.net>
2013-05-14 17:36:15 +0200

    New test for calendar cup

--- test/org/fofo/entity/CalendarCupTest.java
+++ test/org/fofo/entity/CalendarCupTest.java
@@ -1,8 +1,6 @@
 package org.fofo.entity;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 import org.joda.time.DateTime;
 import static org.junit.Assert.*;
@@ -14,8 +12,8 @@ import org.junit.Test;
  * @author jnp2
  */
 public class CalendarCupTest {
-    CalendarGen generator;
-    Competition comp;
+    CalendarCupGen generator;
+    Competition comp;    
     
     @Before
     public void setUp() throws InvalidRequisitsException {
@@ -35,8 +33,6 @@ public class CalendarCupTest {
     }
     
 
-
-
     @Test(expected=MinimumDaysException.class)
     public void testDateException() throws Exception {
         comp.setInici(new DateTime().minusDays(6).toDate());
@@ -49,35 +45,58 @@ public class CalendarCupTest {
         generator = new CalendarCupGen(comp);        
         FCalendar calendar = generator.calculateCalendar();
         int nj = calendar.getNumOfWeekMatches();
-       // assertEquals(4,nj);  
+        assertEquals(4,nj);  
     }
     
     
     @Test   
-    public void testIfAllTeamsParticipateInFirstWeekMatches()  throws Exception {
+    public void testNumMatchesInFirstWeek()  throws Exception {
         generator = new CalendarCupGen(comp);        
         FCalendar calendar = generator.calculateCalendar();
         
         WeekMatches first = calendar.getWeekMatch(0);
         List<Match> listMatch = first.getListOfWeekMatches();
-//expected == listTeams        
-        List<Team> expected = comp.getTeams();
+
+        assertTrue(listMatch.size()==8);
+    }
+    
+    @Test   
+    public void testIfAllTeamsParticipatingInFirstWeekMatchesAreCorrect()  throws Exception {
+        generator = new CalendarCupGen(comp);        
+        FCalendar calendar = generator.calculateCalendar();
         
-//Dividir en 2 tests        
-        if(listMatch.size()*2 != 16) fail();
-         
-//Assert         
+        WeekMatches first = calendar.getWeekMatch(0);
+        List<Match> listMatch = first.getListOfWeekMatches();     
+        List<Team> listTeams = comp.getTeams();      
+        
         for(Match match : listMatch){
             Team local = match.getLocal();
             Team visitant = match.getVisitant();
-//assertTrue(listTeams.contains(local));
-//assertTrue(listTeams.contains(visitant));            
-            if(!expected.contains(local) || !expected.contains(visitant)){
-                fail();
-            }
-        }          
+            assertTrue(listTeams.contains(local));
+            assertTrue(listTeams.contains(visitant));  
+        }
     }
     
+    @Test   
+    public void testIfOurTeamsParticipatingInFirstWeekMatch()  throws Exception {
+        generator = new CalendarCupGen(comp);        
+        FCalendar calendar = generator.calculateCalendar();
+        
+        WeekMatches first = calendar.getWeekMatch(0);
+        List<Match> listMatch = first.getListOfWeekMatches();     
+        List<Team> listTeams = comp.getTeams();      
+        
+        for(Match match : listMatch){
+            Team local = match.getLocal();
+            Team visitant = match.getVisitant();
+            listTeams.remove(local);
+            listTeams.remove(visitant); 
+        }
+        assertTrue(listTeams.isEmpty());
+    }    
+    
+    
+     
 //Test que tots els nostres equips juguen un cop
 //Test que els equips nomes estiguin jugan 1 cop    
 //En cap jornada cap equip juga més d'un partit
@@ -90,12 +109,12 @@ public class CalendarCupTest {
   
         int nmatches = 8;
         for(int i=0; i<4; i++){
-            assertTrue(assertEquals(calendar.getWeekMatch(i),nmatches));
+            assertTrue(assertEqualsMatch(calendar.getWeekMatch(i),nmatches));
             nmatches = nmatches/2;
         }        
     }    
     
-    private boolean assertEquals(WeekMatches match, int nmatches){
+    private boolean assertEqualsMatch(WeekMatches match, int nmatches){
         if(match.getNumberOfMatchs() == nmatches) return true;
         else return false;
     }
