f7cef72e7004c5256713af3ebd38c7d9bdd067c8
Ivan <merola.ivan@gmail.com>
2013-05-18 18:09:41 +0200

    Created CompetitionDAOImplIntegTest

---
+++ test/org/fofo/dao/CompetitionDAOImplIntegTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fofo.dao;
+
+import java.util.Arrays;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import org.fofo.entity.Club;
+import org.fofo.entity.Competition;
+import org.fofo.entity.CompetitionLeague;
+import org.fofo.entity.Team;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class CompetitionDAOImplIntegTest {
+
+    EntityManager em;
+    EntityTransaction transaction;
+    CompetitionDAOImpl competitionDAO;
+    Team team;
+    Competition competition;
+    Club club;
+
+    @Before
+    public void setUp() {
+
+        em = null;
+        transaction = null;
+        competitionDAO = new CompetitionDAOImpl();
+        team = new Team("name");
+        competition = new CompetitionLeague();
+        competition.setName("League1");
+        club = new Club("name");
+        competitionDAO.setEM(em);
+        team.setClub(club);
+
+    }
+
+    //@Test
+    public void addCompetition() throws PersistException {
+
+        competitionDAO.addCompetition(competition);
+        Competition comp = competitionDAO.findCompetitionByName("League1");
+        assertNotNull(comp);
+    }
+
+    //@Test
+    public void addTeam_To_Competition() throws PersistException {
+
+        List<Team> list = Arrays.asList(team);
+
+        competitionDAO.addTeam(competition, team);
+        assertEquals(list, competition.getTeams());
+        assertEquals(competition.getTeams(), 
+                competitionDAO.findCompetitionByName("League1").getTeams());
+
+    }
+
+    @Test(expected = PersistException.class)
+    public void add_incorrect_team_to_competition() throws PersistException {
+        competitionDAO.addTeam(competition, null);
+        
+    }
+    
+    @Test(expected=PersistException.class)
+    public void addTeamtoIncorrectCompetition() throws Exception{
+        competitionDAO.addTeam(null, team);
+    }
+    
+    @Test(expected=PersistException.class)
+    public void addTeamWithoutClubToCompetition() throws Exception{
+        Team team2 = new Team("team2");
+        competitionDAO.addTeam(competition, team2);
+    }
+}
