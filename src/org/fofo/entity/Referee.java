c30488415d24223a161efa4f1c4cb7214fc8a65c
Mohamed <mohamed@mohamed-K53U.(none)>
2013-05-28 21:08:01 +0200

    Defined Referee class

---
+++ src/org/fofo/entity/Referee.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fofo.entity;
+
+import javax.persistence.*;
+
+/**
+ *
+ * @author mohamed
+ */
+@Entity
+@Table (name="REFEREE")
+public class Referee {
+    
+    @Id
+    @Column (name="NIF")
+    private String nif;
+    
+    @Column (name="NAME")
+    private String name;
+    
+    public Referee(){
+    }
+    
+    public Referee(String nif, String name){
+        this.nif = nif;
+        this.name = name;
+    }
+    
+    public String getNif(){
+        return nif;
+    }
+    
+    public void setNif(String nif){
+        this.nif = nif;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+   
+    
+    public String toString(){
+        return "<Name: "+this.name+ ", Nif:" +this.nif+">";
+    }
+}
