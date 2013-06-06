7999f0f447c64c7a078b6be1be5bd4d7797626d4
Jordi Niubo Pomar <jnp2@pl-cap014.udl.net>
2013-06-06 16:29:44 +0200

    New exceptions for match

---
+++ src/org/fofo/dao/IncorrectMatchException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fofo.dao;
+
+/**
+ *
+ * @author jnp2
+ */
+class IncorrectMatchException extends Exception {
+    
+}
