af64a2cd42c264cb0df7f951bdfe48846393f8c0
david <portatil@ubuntu.ubuntu-domain>
2013-06-08 17:53:51 +0200

    New exception

---
+++ src/org/fofo/dao/IncorrectMatchTeamsException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fofo.dao;
+
+/**
+ *
+ * @author portatil
+ */
+class IncorrectMatchTeamsException extends Exception {
+
+    public IncorrectMatchTeamsException() {
+    }
+    
+}
