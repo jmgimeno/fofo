615e1fb2aea013cc8ecb2f53fcab18f779781b6e
Anatoli <anatoli.veselinov@gmail.com>
2013-06-18 17:37:16 +0200

    Added Find competition GUI

--- src/org/fofo/presentation/controller/CompetitionFindAction.java
+++ src/org/fofo/presentation/controller/CompetitionFindAction.java
@@ -2,42 +2,37 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package org.fofo.presentation.controller;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import org.fofo.presentation.view.ClubInsertionForm;
-import org.fofo.services.management.*;
 import javax.swing.JFrame;
+import org.fofo.presentation.view.CompetitionFindForm;
+import org.fofo.services.management.ManagementService;
 
 /**
  *
- * @author josepma
- *
- * CLubInsertionAction is the controller action that launches the form to insert
- * a club.
- *
+ * @author Anatoli 
  */
-public class ClubInsertionAction implements ActionListener {
-
-    JFrame parent;
+public class CompetitionFindAction implements ActionListener{
     ManagementService services;
+    JFrame parent;
 
-    public ClubInsertionAction(JFrame frame, ManagementService services) {
+    public CompetitionFindAction(JFrame frame, ManagementService services) {
         parent = frame;
         this.services = services;
-
     }
 
-    public void actionPerformed(ActionEvent e) {
-
-
+    @Override
+    public void actionPerformed(ActionEvent ae) {
         parent.getContentPane().removeAll();
 
-        ClubInsertionForm form = new ClubInsertionForm(parent, services);
+        CompetitionFindForm form = new CompetitionFindForm(parent, services);
 
         parent.getContentPane().add(form, BorderLayout.CENTER);
         parent.revalidate();
     }
+
 }
---
+++ src/org/fofo/presentation/view/CompetitionInfo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.fofo.presentation.view;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import org.fofo.entity.Competition;
+
+/**
+ *
+ * @author Anatoli Veselinov
+ */
+public class CompetitionInfo extends JPanel{
+    
+    public CompetitionInfo(Competition comp){
+        JLabel l1=new JLabel();
+        JLabel l2=new JLabel();
+        JLabel l3=new JLabel();
+        String compName = comp.getName();
+        String compType = comp.getType().name();
+        String compDate = comp.getInici().toString();
+        
+        l1.setText("Name: "+compName);
+        l2.setText("Type: "+compType);
+        l3.setText("Start date: "+compDate);
+        this.add(l1);
+        this.add(l2);
+        this.add(l3);
+        this.setVisible(true);
+    }
+    
+}
